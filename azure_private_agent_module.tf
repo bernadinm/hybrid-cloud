ed590604c64f27c294d3cf88febebfb10ae9071c
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-03-26 13:58:37 -0700

    adding missing module

---
+++ azure_private_agent_module.tf
@@ -0,0 +1,9 @@
+# Create DCOS Mesos Agent Scripts to execute
+module "azure-dcos-mesos-agent" {
+  source = "git@github.com:mesosphere/enterprise-terraform-dcos//tf_dcos_core"
+  bootstrap_private_ip = "${azurerm_network_interface.bootstrap_nic.private_ip_address}"
+  dcos_install_mode    = "${var.state}"
+  dcos_version         = "${var.dcos_version}"
+  role                 = "dcos-mesos-agent"
+}
+
