a6d90775a36364a5afd9e858ce9d68e4eebceb42
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-06-13 14:08:10 -0700

    changing the default azure instance type for azure

--- azure_variable.tf
+++ azure_variable.tf
@@ -4,7 +4,7 @@ variable "num_of_azure_public_agents" {
 
 variable "azure_public_agent_instance_type" {
   description = "Azure DC/OS Private Agent instance type"
-  default = "Standard_D4_v3"
+  default = "Standard_D4_v2"
 }
 
 variable "num_of_azure_private_agents" {
@@ -13,17 +13,17 @@ variable "num_of_azure_private_agents" {
 
 variable "azure_agent_instance_type" {
   description = "Azure DC/OS Private Agent instance type"
-  default = "Standard_D4_v3"
+  default = "Standard_D4_v2"
 }
 
 variable "azure_bootstrap_instance_type" {
   description = "Azure DC/OS Bootstrap instance type"
-  default = "Standard_D4_v3"
+  default = "Standard_D4_v2"
 }
 
 variable "azure_region" {
   description = "Azure region to launch servers."
-  default     = "UK South"
+  default     = "West US"
 }
 
 variable "azure_admin_username" {
