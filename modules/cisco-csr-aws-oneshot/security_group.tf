171a0eaecb24dcff0b061eceb2560276991b825c
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-06-13 16:57:00 -0700

    added proper security group on aws

--- modules/cisco-csr-aws-oneshot/security_group.tf
+++ modules/cisco-csr-aws-oneshot/security_group.tf
@@ -27,6 +27,12 @@ resource "aws_security_group" "sg_g1_csr1000v" {
         cidr_blocks = ["0.0.0.0/0"]
     }
     ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["10.0.0.0/8"]
+    }
+    ingress {
         from_port = -1
         to_port = -1
         protocol = "icmp"
