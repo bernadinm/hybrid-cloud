57b512cacd64dc249e0e51459f38b31b3943d317
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-10-25 17:47:53 -0700

    aws: added elb for aws new region

---
+++ shared_new_region_public_agent_module.tf
@@ -0,0 +1,39 @@
+# Public Agent Load Balancer Access
+# Adminrouter Only
+resource "aws_elb" "public-agent-elb-remote" {
+  name = "${data.template_file.cluster-name.rendered}-pub-agt-r-elb"
+
+  subnets         = ["${aws_subnet.default_group_1_public.id}","${aws_subnet.default_group_2_public.id}", "${aws_subnet.default_group_3_public.id}"]
+  security_groups = ["${aws_security_group.http-https.id}", "${aws_security_group.internet-outbound.id}"]
+  instances       = ["${aws_instance.remote_public_agent-group-1.*.id}", "${aws_instance.remote_public_agent-group-2.*.id}", "${aws_instance.remote_public_agent-group-3.*.id}"]
+
+  listener {
+    lb_port           = 80
+    instance_port     = 80
+    lb_protocol       = "tcp"
+    instance_protocol = "tcp"
+  }
+
+  listener {
+    lb_port           = 443
+    instance_port     = 443
+    lb_protocol       = "tcp"
+    instance_protocol = "tcp"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 2
+    target = "HTTP:9090/_haproxy_health_check"
+    interval = 5
+  }
+
+  lifecycle {
+    ignore_changes = ["name"]
+  }
+}
+
+output "AWS Remote Public Agent ELB Address" {
+  value = "${aws_elb.public-agent-elb-remote.dns_name}"
+}
