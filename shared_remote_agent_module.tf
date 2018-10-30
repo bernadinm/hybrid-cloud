b09f3b8ded1b46c0cfdbc54f3fdca47c590d0d0b
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-10-30 11:02:03 -0700

    aws-remote: point to open core module

--- shared_remote_agent_module.tf
+++ shared_remote_agent_module.tf
@@ -123,10 +123,11 @@ resource "aws_security_group" "group_private_slave" {
 
 # Create DCOS Mesos Agent Scripts to execute
 module "dcos-remote-mesos-agent" {
-  source = "git@github.com:mesosphere/terraform-dcos-enterprise//tf_dcos_core"
+  source = "github.com/dcos/tf_dcos_core"
   bootstrap_private_ip = "${aws_instance.bootstrap.private_ip}"
   dcos_install_mode    = "${var.state}"
   dcos_version         = "${var.dcos_version}"
+  dcos_type            = "${var.dcos_type}"
   role                 = "dcos-mesos-agent"
 }
 
